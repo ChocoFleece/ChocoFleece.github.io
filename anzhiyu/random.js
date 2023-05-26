374a4685a56d735d88d6845d36e4be83b4a361c5
ChocoFleece <zhongzi2333@gmail.com>
2023-05-26 19:36:23 +0900

    Site updated: 2023-05-26 19:36:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/26/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/23/1-hello,world!/","2023/05/25/2-tokyounotenki/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
